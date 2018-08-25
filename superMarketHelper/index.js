8a069a5bfb943b475cd031589a48a676948d8a7c
AntonioRdzNav <antoniordznav@outlook.com>
2018-08-24 20:44:48 -0500

    Initial Commit

---
+++ superMarketHelper/index.js
@@ -0,0 +1,16 @@
+// Import library to help create a component
+import { AppRegistry } from 'react-native';
+import App from './App';
+// "./" means start in current directory
+// import Header from './src/components/header';
+
+// Create a component
+// const App = () => (
+    {/* <Header headerText={'Shop List'}/> */}
+// );
+
+
+// Render component 
+// ReactNative.AppRegistry.registerComponent('superMarketHelper', () => App);
+AppRegistry.registerComponent('superMarketHelper', () => App);
+ 
