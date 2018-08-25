c07fb2a608f4c45f2dc5e5b43a8a049dd500f42c
AntonioRdzNav <antoniordznav@outlook.com>
2018-08-25 03:37:30 -0500

    All basic components added
    Button, Card, CardSection, Header, ProducDetail, Products

--- superMarketHelper/src/components/ProductDetail.js
+++ superMarketHelper/src/components/ProductDetail.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const ProductDetail = (props) => {
     return(
         <Card>
             <CardSection>
                 <Text> {props.product.title} </Text>
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => console.log("PRESSED")}>  
+                    Buy Now 
+                </Button>
             </CardSection>
         </Card>
     );
