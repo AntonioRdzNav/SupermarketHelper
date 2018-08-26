a40794e80527dd1fdaf2a7407b484098f8207508
AntonioRdzNav <antoniordznav@outlook.com>
2018-08-26 03:02:59 -0500

    Commit for Beto and Alex

--- superHelper/App.js
+++ superHelper/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Scanner from './src/components/Scanner';
 import Main from './src/Main';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Main/>
+    // return (
+    //   <Scanner/>
+    // );
+    return(
+        <Main/>
     );
   }
 }
